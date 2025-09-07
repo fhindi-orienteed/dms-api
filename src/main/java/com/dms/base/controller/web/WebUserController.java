9f71f5d0391415d858a1d33090298cfa6d7375e3
Fathi Hindi <162419512+fhindi-orienteed@users.noreply.github.com>
2025-09-07 13:49:04 +0300

    Web User API

--- src/main/java/com/dms/base/controller/web/WebUserController.java
+++ src/main/java/com/dms/base/controller/web/WebUserController.java
@@ -1,9 +1,12 @@
 package com.dms.base.controller.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dms.base.controller.common.UserController;
+import com.dms.base.model.User;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -12,4 +15,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Web API", description = "Endpoints for Web Users API")
 public class WebUserController extends UserController {
 
+    @GetMapping("/current")
+    public ResponseEntity<?> getCurrentUser() {
+        User user = userService.getCurrentUser();
+        return ResponseEntity.ok(userMapper.mapToWebResponse(user));
+    }
 }
---
+++ src/main/java/com/dms/base/dto/response/web/WebUserResponse.java
@@ -0,0 +1,7 @@
+package com.dms.base.dto.response.web;
+
+import com.dms.base.dto.response.common.UserResponse;
+
+public class WebUserResponse extends UserResponse {
+
+}

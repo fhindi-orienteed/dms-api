68a1fddcc74165539d6e3f565c218c819d41f5ae
awadsholi <145344626+awadsholi@users.noreply.github.com>
2025-09-08 16:24:20 +0300

    Update WebUserResponse.java

--- src/main/java/com/dms/base/dto/response/web/WebUserResponse.java
+++ src/main/java/com/dms/base/dto/response/web/WebUserResponse.java
@@ -1,6 +1,108 @@
 package com.dms.base.dto.response.web;
 import com.dms.base.dto.response.common.UserResponse;
 
-public class WebUserResponse extends UserResponse {
+public class WebUserResponse {
+    private Long id;
+    private Date createdDate;
+    private String userName;
+    private String password;
+    private Integer status;
+    private String role;
+    private Integer passwordRetries;
+    private Integer passwordExpired;
+    private Integer passwordCreation;
+    private Integer timeout;
 
+    private Date lastSession;
+
+    public WebUserResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getLastSession() {
+        return lastSession;
+    }
+
+    public void setLastSession(Date lastSession) {
+        this.lastSession = lastSession;
+    }
+
+    public Integer getPasswordRetries() {
+        return passwordRetries;
+    }
+
+    public void setPasswordRetries(Integer passwordRetries) {
+        this.passwordRetries = passwordRetries;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getPasswordExpired() {
+        return passwordExpired;
+    }
+
+    public void setPasswordExpired(Integer passwordExpired) {
+        this.passwordExpired = passwordExpired;
+    }
+
+    public Integer getPasswordCreation() {
+        return passwordCreation;
+    }
+
+    public void setPasswordCreation(Integer passwordCreation) {
+        this.passwordCreation = passwordCreation;
+    }
+
+    public Integer getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(Integer timeout) {
+        this.timeout = timeout;
+    }
 }

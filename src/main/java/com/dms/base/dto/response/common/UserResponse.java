7e2b95b15812c7cea2fb0052af76298938145350
Fathi Hindi <162419512+fhindi-orienteed@users.noreply.github.com>
2025-09-18 14:20:22 +0300

    Fix api

--- src/main/java/com/dms/base/dto/response/common/UserResponse.java
+++ src/main/java/com/dms/base/dto/response/common/UserResponse.java
@@ -8,10 +8,6 @@ public class UserResponse {
     private String userName;
     private Integer status;
     private String role;
-    private Integer passwordRetries;
-    private Integer passwordExpired;
-    private Integer passwordCreation;
-    private Integer timeout;
     private Date lastSession;
 
     public Long getId() {
@@ -52,38 +48,6 @@ public class UserResponse {
 
     public void setRole(String role) {
         this.role = role;
-    }
-
-    public Integer getPasswordRetries() {
-        return passwordRetries;
-    }
-
-    public void setPasswordRetries(Integer passwordRetries) {
-        this.passwordRetries = passwordRetries;
-    }
-
-    public Integer getPasswordExpired() {
-        return passwordExpired;
-    }
-
-    public void setPasswordExpired(Integer passwordExpired) {
-        this.passwordExpired = passwordExpired;
-    }
-
-    public Integer getPasswordCreation() {
-        return passwordCreation;
-    }
-
-    public void setPasswordCreation(Integer passwordCreation) {
-        this.passwordCreation = passwordCreation;
-    }
-
-    public Integer getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(Integer timeout) {
-        this.timeout = timeout;
     }
 
     public Date getLastSession() {

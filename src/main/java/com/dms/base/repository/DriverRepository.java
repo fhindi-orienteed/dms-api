7687fb8539325d48c0be409a843d70d8703dc8aa
awadsholi <145344626+awadsholi@users.noreply.github.com>
2025-09-18 17:16:30 +0300

    Mobile_User_Profile

--- src/main/java/com/dms/base/repository/DriverRepository.java
+++ src/main/java/com/dms/base/repository/DriverRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver,Long>{
-    
+    Driver findByUserId(long userId);
 }
